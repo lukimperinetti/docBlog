678568cf3356ef8e17c94f58cf94a7f9a2e13c07
lukimperinetti <luk.imperinetti@epitech.eu>
2024-01-09 10:17:44 +0100

    first commit

---
+++ src/DocBlog/views.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+from django.shortcuts import render
+
+
+def index(request):
+
+    date = datetime.now()
+
+    return render(request, 'index.html', context={"date": date})
